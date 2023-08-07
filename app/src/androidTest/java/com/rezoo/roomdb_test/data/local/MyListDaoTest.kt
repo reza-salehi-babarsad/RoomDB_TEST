d65e1343d7783190acdb00632e8ae2835610b0ec
reza <rezasalehibabarsad@hotmail.com>
2023-08-07 23:49:08 +0330

    DaoClass2

--- app/src/androidTest/java/com/rezoo/roomdb_test/data/local/MyListDaoTest.kt
+++ app/src/androidTest/java/com/rezoo/roomdb_test/data/local/MyListDaoTest.kt
@@ -1,4 +1,39 @@
 package com.rezoo.roomdb_test.data.local
 
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import androidx.test.filters.SmallTest
+import org.junit.After
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+@SmallTest
 class MyListDaoTest {
+
+
+    private lateinit var dataBase: MyListDataBase
+    private lateinit var dao: MyListDao
+
+    @Before
+    fun setup(){
+
+        dataBase =Room.inMemoryDatabaseBuilder(
+            ApplicationProvider.getApplicationContext(),
+            MyListDataBase::class.java
+        ).allowMainThreadQueries().build()
+        dao= dataBase.myListDao()
+    }
+
+    @After
+    fun teardown(){
+        dataBase.close()
+    }
+
+    @Test
+    fun insertMyListItem() =
+
+
 }
