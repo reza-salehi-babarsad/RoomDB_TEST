4d331912782e131e472e2ca5ccc6eed0308d665f
reza <rezasalehibabarsad@hotmail.com>
2023-08-08 23:46:37 +0330

    LiveDataUtilAndroidTest

--- app/src/androidTest/java/com/rezoo/roomdb_test/data/local/MyListDaoTest.kt
+++ app/src/androidTest/java/com/rezoo/roomdb_test/data/local/MyListDaoTest.kt
@@ -4,6 +4,8 @@ import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
 import org.junit.Test
@@ -32,8 +34,12 @@ class MyListDaoTest {
         dataBase.close()
     }
 
+    @OptIn(ExperimentalCoroutinesApi::class)
     @Test
-    fun insertMyListItem() =
+    fun insertMyListItem() = runBlockingTest {
+
+        
+    }
 
 
 }
