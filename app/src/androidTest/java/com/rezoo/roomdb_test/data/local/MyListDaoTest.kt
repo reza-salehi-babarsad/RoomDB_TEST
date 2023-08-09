13ff7ead2e52c39cd4219ed83c9802bc47401f7a
reza <rezasalehibabarsad@hotmail.com>
2023-08-09 15:55:28 +0330

    insert item Test

--- app/src/androidTest/java/com/rezoo/roomdb_test/data/local/MyListDaoTest.kt
+++ app/src/androidTest/java/com/rezoo/roomdb_test/data/local/MyListDaoTest.kt
@@ -1,20 +1,26 @@
 package com.rezoo.roomdb_test.data.local
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
 import androidx.room.Room
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.SmallTest
+import com.google.common.truth.Truth.assertThat
+import com.rezoo.roomdb_test.getOrAwaitValue
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.test.runBlockingTest
 import org.junit.After
 import org.junit.Before
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
+@ExperimentalCoroutinesApi
 @RunWith(AndroidJUnit4::class)
 @SmallTest
 class MyListDaoTest {
 
+    @get:Rule
+    var instantTaskExecutorRule = InstantTaskExecutorRule()
 
     private lateinit var dataBase: MyListDataBase
     private lateinit var dao: MyListDao
@@ -34,10 +40,15 @@ class MyListDaoTest {
         dataBase.close()
     }
 
-    @OptIn(ExperimentalCoroutinesApi::class)
+
     @Test
     fun insertMyListItem() = runBlockingTest {
+        val myList:MyList = MyList("name",1,1f,"Url",1)
+        dao.insertMyList(myList)
 
+        val allMyList = dao.observeAllMyList().getOrAwaitValue()
+
+        assertThat(allMyList).contains(myList)
         
     }
 
